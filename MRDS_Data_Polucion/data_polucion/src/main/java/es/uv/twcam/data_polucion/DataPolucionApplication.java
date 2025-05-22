9ae7ef7dd9f586756048faf1e0e6b8798f8beb62
diegoseg15 <diegofersv@gmail.com>
2025-05-23 01:44:33 +0200

    :tada: Initialize data_polucion microservice project

---
+++ MRDS_Data_Polucion/data_polucion/src/main/java/es/uv/twcam/data_polucion/DataPolucionApplication.java
@@ -0,0 +1,13 @@
+package es.uv.twcam.data_polucion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPolucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPolucionApplication.class, args);
+	}
+
+}
---
+++ MRDS_Data_Polucion/data_polucion/src/test/java/es/uv/twcam/data_polucion/DataPolucionApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uv.twcam.data_polucion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPolucionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
