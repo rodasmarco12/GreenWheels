5813d7add32c07ba7895306c90ff58402536949c
diegoseg15 <diegofersv@gmail.com>
2025-05-22 19:51:23 +0200

    :sparkles: Add reactive StationService with CRUD operations

---
+++ MRDS_Polucion/src/main/java/es/uv/twcam/polucion/services/StationService.java
@@ -0,0 +1,33 @@
+package es.uv.twcam.polucion.services;
+
+import org.springframework.stereotype.Service;
+
+import es.uv.twcam.polucion.domain.Station;
+import es.uv.twcam.polucion.repositories.StationRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class StationService {
+    private final StationRepository stationRepository;
+
+    public StationService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public Flux<Station> findAll() {
+        return stationRepository.findAll();
+    }
+
+    public Mono<Station> save(Station s) {
+        return stationRepository.save(s);
+    }
+
+    public Mono<Void> delete(String id) {
+        return stationRepository.deleteById(id);
+    }
+
+    public Mono<Station> findById(String id) {
+        return stationRepository.findById(id);
+    }
+}
