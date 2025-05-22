ade9afb25aa453231fac4f800c6b77adbdb90f68
diegoseg15 <diegofersv@gmail.com>
2025-05-22 19:28:53 +0200

    :recycle: Make StationRepository reactive using ReactiveCrudRepository

--- MRDS_Polucion/src/main/java/es/uv/twcam/polucion/repositories/StationRepository.java
+++ MRDS_Polucion/src/main/java/es/uv/twcam/polucion/repositories/StationRepository.java
@@ -1,11 +1,11 @@
 package es.uv.twcam.polucion.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import es.uv.twcam.polucion.domain.Station;
 
 // Repositorio de MySQL
 @Repository
-public interface StationRepository extends JpaRepository<Station, String> {
+public interface StationRepository extends ReactiveCrudRepository<Station, String> {
 }
