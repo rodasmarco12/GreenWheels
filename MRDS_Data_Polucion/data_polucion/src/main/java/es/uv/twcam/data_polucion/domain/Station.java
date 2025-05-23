a20bb49d7ae713e56f4a9a215ba29b7b5ffbba51
diegoseg15 <diegofersv@gmail.com>
2025-05-23 10:51:15 +0200

    :label: Add Station entity for MySQL relational mapping

---
+++ MRDS_Data_Polucion/data_polucion/src/main/java/es/uv/twcam/data_polucion/domain/Station.java
@@ -0,0 +1,66 @@
+package es.uv.twcam.data_polucion.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.relational.core.mapping.Column;
+
+// Tabla "station" de MySQL
+@Table(name = "station")
+public class Station {
+
+    @Id
+    private String id;
+
+    @Column("direction")
+    private String direction;
+
+    @Column("latitude")
+    private float latitude;
+
+    @Column("longitude")
+    private float longitude;
+
+    public Station() {
+    }
+
+    public Station(String direccion, float latitude, float longitude) {
+        this.direction = direccion;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // Getters y Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direction;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direction = direccion;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+}
