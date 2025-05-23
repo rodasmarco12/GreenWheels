2698ef32081d3d748d6ef60dd16a0be6bbce7fce
diegoseg15 <diegofersv@gmail.com>
2025-05-23 10:54:26 +0200

    :sparkles: Implement ReadingService with reactive persistence and queries

---
+++ MRDS_Data_Polucion/data_polucion/src/main/java/es/uv/twcam/data_polucion/services/StationService.java
@@ -0,0 +1,47 @@
+package es.uv.twcam.data_polucion.services;
+
+import org.springframework.stereotype.Service;
+
+import es.uv.twcam.data_polucion.domain.Station;
+import es.uv.twcam.data_polucion.repositories.StationRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class StationService {
+    private final StationRepository stationRepository;
+
+    public StationService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    // Obtener todas las estaciones
+    public Flux<Station> findAll() {
+        return stationRepository.findAll();
+    }
+
+    // Guardar una estación
+    public Mono<Station> save(Station s) {
+        return stationRepository.save(s);
+    }
+
+    // Eliminar por id
+    public Mono<Void> delete(String id) {
+        return stationRepository.deleteById(id);
+    }
+
+    // Buscar por id
+    public Mono<Station> findById(String id) {
+        return stationRepository.findById(id);
+    }
+
+    public Mono<Station> update(String id, Station s) {
+        return this.stationRepository.findById(id)
+                .flatMap(station -> {
+                    station.setDireccion(s.getDireccion());
+                    station.setLatitude(s.getLatitude());
+                    station.setLongitude(s.getLongitude());
+                    return stationRepository.save(station);
+                });
+    }
+}
