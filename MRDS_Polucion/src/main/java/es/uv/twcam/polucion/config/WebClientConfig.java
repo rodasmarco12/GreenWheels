1edb023cf62af3c7afb9b06ba33ddc8161f96c34
diegoseg15 <diegofersv@gmail.com>
2025-05-23 10:55:10 +0200

    :wrench: Configure WebClient with base URL for data-polucion service

---
+++ MRDS_Polucion/src/main/java/es/uv/twcam/polucion/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package es.uv.twcam.polucion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().baseUrl("http://localhost:8081/api/v1/data-polucion").build();
+    }
+}
