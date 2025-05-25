ade875163f20e33458067a6910229a1d29c75ce8
Marco Rodas <marcoeduardorodas@gmail.com>
2025-05-25 19:40:49 +0200

    implement scheduler that sends bicycle events to the corresponding microservice

---
+++ MRDS_Aparcamiento/src/test/java/uv/es/twcam/aparcamiento/AparcamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package uv.es.twcam.aparcamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AparcamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
