4dab49af1fbbb4bc173f0e149725ae18c2b94901
diegoseg15 <diegofersv@gmail.com>
2025-05-26 00:25:46 +0200

    :tada: Create new project for cronjob to automatically request pollution API

---
+++ MRDS_Estacion/src/main/java/uv/es/twcam/estacion/EstacionApplication.java
@@ -0,0 +1,13 @@
+package uv.es.twcam.estacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionApplication.class, args);
+	}
+
+}
---
+++ MRDS_Estacion/src/test/java/uv/es/twcam/estacion/EstacionApplicationTests.java
@@ -0,0 +1,13 @@
+package uv.es.twcam.estacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
