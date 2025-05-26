db01f49097f5da751ba3dccc5ff6aaa4c99860b0
Marco Rodas <marcoeduardorodas@gmail.com>
2025-05-26 16:48:09 +0200

    Add Security to Bicicletas Service

--- MRDS_Bicicletas/src/main/java/es/uv/twcam/bicicletas/config/OpenAPIConfig.java
+++ MRDS_Bicicletas/src/main/java/es/uv/twcam/bicicletas/config/OpenAPIConfig.java
@@ -1,7 +1,11 @@
 package es.uv.twcam.bicicletas.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +13,19 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-            .info(new Info()
-                .title("Access API for Bicycles")
-                .version("1.0")
-                .description("API for managing bike parking data"));
+                .info(new Info()
+                        .title("Access API for Bicycles")
+                        .version("1.0")
+                        .description("API for managing bike parking data"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
