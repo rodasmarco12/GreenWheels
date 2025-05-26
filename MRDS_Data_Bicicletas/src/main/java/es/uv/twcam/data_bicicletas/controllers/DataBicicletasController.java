32f1644a6d2f98070c010df10ed34e80c32a1224
Marco Rodas <marcoeduardorodas@gmail.com>
2025-05-26 17:28:33 +0200

    add banners and delete unnecessary imports

--- MRDS_Data_Bicicletas/src/main/java/es/uv/twcam/data_bicicletas/controllers/DataBicicletasController.java
+++ MRDS_Data_Bicicletas/src/main/java/es/uv/twcam/data_bicicletas/controllers/DataBicicletasController.java
@@ -2,14 +2,14 @@ package es.uv.twcam.data_bicicletas.controllers;
 
 import java.net.URI;
 import java.time.Instant;
-import java.util.HashMap;
+
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.support.WebExchangeBindException;
+
 
 import es.uv.twcam.data_bicicletas.DTO.AverageResult;
 import es.uv.twcam.data_bicicletas.domain.Aparcamiento;
@@ -28,7 +28,7 @@ import es.uv.twcam.data_bicicletas.services.EventoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.Valid;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import es.uv.twcam.data_bicicletas.DTO.AparcamientoDTO;
